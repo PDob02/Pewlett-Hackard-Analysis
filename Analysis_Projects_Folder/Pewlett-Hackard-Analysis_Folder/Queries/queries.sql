9f2537939d05136b0e8ce8c6deaef3f661628dfc
PDob02 <patrickdobry@gmail.com>
2022-02-05 01:29:42 -0600

    adding queries

--- Analysis_Projects_Folder/Pewlett-Hackard-Analysis_Folder/Queries/queries.sql
+++ Analysis_Projects_Folder/Pewlett-Hackard-Analysis_Folder/Queries/queries.sql
@@ -1,61 +1,3 @@
--- -- Creating tables for PH-EmployeeDB
--- -- CREATE TABLE employees (
--- -- 	 emp_no INT NOT NULL,
--- --      birth_date DATE NOT NULL,
--- --      first_name VARCHAR NOT NULL,
--- --      last_name VARCHAR NOT NULL,
--- --      gender VARCHAR NOT NULL,
--- --      hire_date DATE NOT NULL,
--- --      PRIMARY KEY (emp_no)
--- -- );
-
--- -- CREATE TABLE dept_manager (
--- -- dept_no VARCHAR(4) NOT NULL,
--- --     emp_no INT NOT NULL,
--- --     from_date DATE NOT NULL,
--- --     to_date DATE NOT NULL,
--- -- FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
--- -- FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
--- --     PRIMARY KEY (emp_no, dept_no)
--- -- );
-
--- -- CREATE TABLE salaries (
--- --   emp_no INT NOT NULL,
--- --   salary INT NOT NULL,
--- --   from_date DATE NOT NULL,
--- --   to_date DATE NOT NULL,
--- --   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
--- --   PRIMARY KEY (emp_no)
--- -- );
--- SELECT * FROM departments;
-
--- CREATE TABLE dept_emp(
--- -- dept_no VARCHAR(4) NOT NULL,
--- -- 	to_date DATE NOT NULL,
--- -- 	from_date DATE NOT NULL,
--- -- 	PRIMARY KEY (emp_no, dept_no)
--- -- );
-
--- CREATE TABLE dept_emp(
--- dept_no VARCHAR(4) NOT NULL,
---     emp_no INT NOT NULL,
---     from_date DATE NOT NULL,
---     to_date DATE NOT NULL,
--- FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
--- FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
---     PRIMARY KEY (emp_no, dept_no)
--- );
-
--- Creating tables for PH-EmployeeDB
--- CREATE TABLE departments (
---      dept_no VARCHAR(4) NOT NULL,
---      dept_name VARCHAR(40) NOT NULL,
---      PRIMARY KEY (dept_no),
---      UNIQUE (dept_name)
--- );
--- -- SELECT * from departments;
--- SELECT * from dept_emp;
-
 -- Creating tables for PH-EmployeeDB
 -- CREATE TABLE departments (
 --      dept_no VARCHAR(4) NOT NULL,
@@ -75,13 +17,13 @@
 -- FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 --     PRIMARY KEY (emp_no, dept_no)
 -- );
--- SELECT * from dept_emp;
--- SELECT * from dept_manager;
 -- SELECT * from dept_emp; did not complete properly
--- SELECT * from dept_manager; did not complete properly
+-- SELECT * from dept_manager;
 -- SELECT * from employees;
 -- SELECT * from salaries;
 -- SELECT * from titles; did not complete properly
+
+
 -- CREATE TABLE dept_emp (
 -- dept_no VARCHAR NOT NULL,
 -- emp_no INT NOT NULL,
@@ -124,6 +66,17 @@
 -- SELECT * from salaries;
 -- SELECT * from titles; did not complete properly
 
+-- select * from titles
+-- select * from departments
+
+-- SELECT first_name, last_name
+-- FROM employees
+-- WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- SELECT first_name, last_name
+-- FROM employees
+-- WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
 
 -- SELECT first_name, last_name
 -- FROM employees
@@ -142,5 +95,29 @@
 -- Retirement eligibility
 -- SELECT first_name, last_name
 -- FROM employees
+-- WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+-- SELECT first_name, last_name
+-- FROM employees
 -- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 -- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+-- SELECT COUNT(first_name)
+-- FROM employees
+-- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- SELECT first_name, last_name
+-- FROM employees
+-- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- SELECT first_name, last_name
+-- INTO retirement_info
+-- FROM employees
+-- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- SELECT * FROM retirement_info;
