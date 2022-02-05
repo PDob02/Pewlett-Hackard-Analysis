cd95bee6212312732efbe0a969471b7a6d063a55
PDob02 <patrickdobry@gmail.com>
2022-02-05 17:01:27 -0600

    Redoing 7.3.5 Create Additional Lists

--- Analysis_Projects_Folder/Pewlett-Hackard-Analysis_Folder/Queries/queries.sql
+++ Analysis_Projects_Folder/Pewlett-Hackard-Analysis_Folder/Queries/queries.sql
@@ -364,3 +364,73 @@
 -- LEFT JOIN dept_emp as de
 -- ON ce.emp_no = de.emp_no
 -- GROUP BY de.dept_no;
+
+
+-- -- Employee count by department number
+-- SELECT COUNT(ce.emp_no), de.dept_no
+-- FROM current_emp as ce
+-- LEFT JOIN dept_emp as de
+-- ON ce.emp_no = de.emp_no
+-- GROUP BY de.dept_no
+-- ORDER BY de.dept_no;
+
+
+-- COPY (SELECT COUNT(ce.emp_no), de.dept_no
+-- FROM current_emp as ce
+-- LEFT JOIN dept_emp as de
+-- ON ce.emp_no = de.emp_no
+-- GROUP BY de.dept_no
+-- ORDER BY de.dept_no)
+-- TO 'E:\Class_Folder\current_emp.csv'
+-- DELIMITER ',' CSV HEADER;
+-- SELECT * FROM salaries
+-- ORDER BY to_date DESC;
+
+
+-- COPY (SELECT COUNT(ce.emp_no), de.dept_no
+-- FROM current_emp as ce
+-- LEFT JOIN dept_emp as de
+-- ON ce.emp_no = de.emp_no
+-- GROUP BY de.dept_no
+-- ORDER BY de.dept_no)
+-- TO 'E:\Class_Folder\current_emp.csv'
+-- DELIMITER ',' CSV HEADER;
+-- SELECT * FROM salaries
+-- ORDER BY to_date DESC;
+
+-- SELECT emp_no, first_name, last_name
+-- INTO retirement_info
+-- FROM employees
+-- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- SELECT * FROM salaries
+-- -- ORDER BY to_date DESC;
+-- SELECT e.emp_no,
+--     e.first_name,
+-- e.last_name,
+--     e.gender,
+--     s.salary,
+--     de.to_date
+-- INTO emp_info
+-- FROM employees
+-- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- -- select * from emp_info
+
+-- SELECT e.emp_no,
+--     e.first_name,
+-- e.last_name,
+--     e.gender,
+--     s.salary,
+--     de.to_date
+-- INTO emp_info
+-- FROM employees as e
+-- INNER JOIN salaries as s
+-- ON (e.emp_no = s.emp_no)
+-- INNER JOIN dept_emp as de
+-- ON (e.emp_no = de.emp_no)
+-- WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+--      AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+-- 	      AND (de.to_date = '9999-01-01');
