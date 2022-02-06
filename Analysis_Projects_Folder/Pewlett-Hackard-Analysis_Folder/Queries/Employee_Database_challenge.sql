d8d35b96dc2e9e134fdeed83a2f436ea13a151a5
PDob02 <patrickdobry@gmail.com>
2022-02-06 11:01:15 -0600

    adding after running unique_titles

--- Analysis_Projects_Folder/Pewlett-Hackard-Analysis_Folder/Queries/Employee_Database_challenge.sql
+++ Analysis_Projects_Folder/Pewlett-Hackard-Analysis_Folder/Queries/Employee_Database_challenge.sql
@@ -1,8 +1,8 @@
---Employee_Database_challenge.sql 
-SELECT emp_no, first_name,last_name
-FROM employees
-SELECT title, from_date, to_date
-FROM titles
+-- --Employee_Database_challenge.sql 
+-- SELECT emp_no, first_name,last_name
+-- FROM employees
+-- SELECT title, from_date, to_date
+-- FROM titles
 
 -- SELECT employees.emp_no, employees.first_name,employees.last_name, titles.title, titles.from_date, titles.to_date
 -- -- --INTO new_table
@@ -15,10 +15,51 @@ FROM titles
 -- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 -- ORDER BY emp_no ASC;
 --step 7
-SELECT employees.emp_no, employees.first_name,employees.last_name, titles.title, titles.from_date, titles.to_date
-INTO retirement_titles
-FROM employees
-LEFT JOIN titles ON employees.emp_no = titles.emp_no
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY emp_no ASC;
+-- SELECT employees.emp_no, employees.first_name,employees.last_name, titles.title, titles.from_date, titles.to_date
+-- INTO retirement_titles
+-- FROM employees
+-- LEFT JOIN titles ON employees.emp_no = titles.emp_no
+-- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- ORDER BY emp_no ASC;
 --exported file manually
+-- Use Dictinct with Orderby to remove duplicate rows
+-- SELECT DISTINCT ON (______) _____,
+-- ______,
+-- ______,
+-- ______
+
+-- INTO nameyourtable
+-- FROM _______
+-- WHERE _______
+-- ORDER BY _____, _____ DESC;
+
+-- SELECT DISTINCT ON (emp_no) emp_no,
+-- first_name,
+-- last_name,
+-- title
+-- -- INTO nameyourtable
+-- FROM retirement_titles
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+-- INTO most_recent_title
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY _____, _____ DESC;
+
+-- SELECT DISTINCT ON (emp_no) emp_no,
+-- first_name,
+-- last_name,
+-- title
+-- -- INTO nameyourtable
+-- FROM retirement_titles
+-- SELECT DISTINCT ON (emp_no) emp_no,
+-- first_name,
+-- last_name,
+-- title
+-- INTO Unique_Titles
+-- FROM retirement_titles
+-- WHERE to_date = '9999-01-01'
+
+select * from unique_titles
