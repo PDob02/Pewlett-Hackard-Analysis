1492007c012bf1a4f73c88978adc51614648325f
PDob02 <patrickdobry@gmail.com>
2022-02-05 20:43:48 -0600

    Adding before module

--- Analysis_Projects_Folder/Pewlett-Hackard-Analysis_Folder/Queries/queries.sql
+++ Analysis_Projects_Folder/Pewlett-Hackard-Analysis_Folder/Queries/queries.sql
@@ -434,3 +434,73 @@
 -- WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 --      AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 -- 	      AND (de.to_date = '9999-01-01');
+-- SELECT * FROM salaries
+-- ORDER BY to_date DESC;
+
+-- SELECT emp_no,
+--     first_name,
+-- last_name,
+--     gender
+-- INTO emp_info
+-- FROM employees
+-- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- select * from emp_info
+-- SELECT e.emp_no,
+--     e.first_name,
+-- e.last_name,
+--     e.gender,
+--     s.salary,
+--     de.to_date
+-- --INTO emp_info
+-- FROM employees as e
+-- INNER JOIN salaries as s
+-- ON (e.emp_no = s.emp_no)
+-- INNER JOIN dept_emp as de
+-- ON (e.emp_no = de.emp_no)
+-- WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+--      AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+-- 	 AND (de.to_date = '9999-01-01');
+
+-- -- List of managers per department
+-- SELECT  dm.dept_no,
+--         d.dept_name,
+--         dm.emp_no,
+--         ce.last_name,
+--         ce.first_name,
+--         dm.from_date,
+--         dm.to_date
+-- --INTO manager_info
+-- FROM dept_manager AS dm
+--     INNER JOIN departments AS d
+--         ON (dm.dept_no = d.dept_no)
+--     INNER JOIN current_emp AS ce
+--         ON (dm.emp_no = ce.emp_no);
+
+-- SELECT ce.emp_no,
+-- ce.first_name,
+-- ce.last_name,
+-- d.dept_name
+-- -- INTO dept_info
+-- FROM current_emp as ce
+-- INNER JOIN dept_emp AS de
+-- ON (ce.emp_no = de.emp_no)
+-- INNER JOIN departments AS d
+-- ON (de.dept_no = d.dept_no);
+
+-- Create a query that will return only the information relevant to the Sales team. The requested list includes:
+
+-- Employee numbers
+-- Employee first name
+-- Employee last name
+-- Employee department name
+
+-- Create another query that will return the following information for the Sales and Development teams:
+
+-- Employee numbers
+-- Employee first name
+-- Employee last name
+-- Employee department name
+-- Hint: You'll need to use the IN condition with the WHERE clause. See the PostgreSQL documentation (Links to an external site.) for additional information.
+
+-- The IN condition is necessary because you're creating two items in the same column.
